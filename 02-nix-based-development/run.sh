84183603cfff502fa3583527b62ec86fb991118b
Peter Simons <simons@cryp.to>
2017-10-10 12:55:49 +0200

    02-nix-based-development: simple dev example

---
+++ 02-nix-based-development/run.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+set -e
+cd "$(dirname "$0")"
+
+# Get the source code of the desired Haskell package.
+cabal get nix-derivation-1.0.0
+cd nix-derivation-1.0.0
+
+# Enter a temporary development environment from Nixpkgs.
+nix-shell "<nixpkgs>" -A haskellPackages.nix-derivation.env --run "cabal configure"
+cabal build
+
+# Generate a development environment with cabal2nix.
+cabal2nix --shell . >shell.nix
+cabal clean
+nix-shell --run "cabal configure --enable-test"
+cabal test
+
+# Interactive development.
+: cabal repl lib:nix-derivation

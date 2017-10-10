5a6f03e50ebb6e0a451a769895acba7e77645211
Peter Simons <simons@cryp.to>
2017-10-10 12:25:41 +0200

    01-normal-haskell-development: simple dev example

---
+++ 01-normal-haskell-development/run.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -e
+
+# Get the source code of the desired Haskell package.
+cabal update
+cabal get mtl-compat-0.2.1.3
+cd mtl-compat-0.2.1.3
+
+# Build it with cabal-install.
+cabal new-build
+
+# Build it with stack.
+stack config set system-ghc --global true
+stack init
+stack build
+
+# Build it with a more recent version of GHC.
+nix-shell -p haskell.compiler.ghc821 --run "cabal new-build"

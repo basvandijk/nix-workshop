c66e507fca80ef968e226385f56836407925d05a
Peter Simons <simons@cryp.to>
2017-10-11 18:40:07 +0200

    00-introduction-to-nix: add fibunacci example

--- 00-introduction-to-nix/run.sh
+++ 00-introduction-to-nix/run.sh
@@ -7,8 +7,14 @@ nix-instantiate --eval -E '"hello"'                      # prints: "hello"
 nix-instantiate --eval -E '1 + 2'                        # prints: 3
 nix-instantiate --eval -E '[ "hello" (1 + 2) ]'          # prints: [ "hello" <CODE> ]
 nix-instantiate --eval --strict -E '[ "hello" (2 - 1) ]' # prints: [ "hello" 3 ]
-nix-instantiate --eval -E ./run.sh                       # paths are special
-nix-instantiate --eval -E http://cryp.to/                # urls strings
+nix-instantiate --eval --strict -E '
+  with import <nixpkgs> {};
+  let
+    fib = n: if n == 0 then 0 else
+             if n == 1 then 1 else
+             fib (n - 2) + fib (n - 1);
+  in
+    map fib (lib.range 0 10)'
 
 # Create a store derivation.
 nix-instantiate 2>/dev/null -E '

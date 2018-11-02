2ef98a32d08478865d4e5567d5fd7e783e764f76
Peter Simons <simons@cryp.to>
2018-11-02 19:07:08 +0100

    04-ghc-with-packages: update code for cabal-install 2.4.x

--- 04-ghc-with-packages/run.sh
+++ 04-ghc-with-packages/run.sh
@@ -4,27 +4,27 @@ set -eu
 cd "$(dirname "$0")"
 
 # Get the source code of the desired Haskell package.
-rm -rf mustache-2.2.3
-cabal get mustache-2.2.3
-cd mustache-2.2.3
+rm -rf mustache-2.3.0
+cabal get mustache-2.3.0
+cd mustache-2.3.0
 
 # Construct a GHC environment with all necessary packages.
-nix-shell --run "cabal configure --enable-test"  -p '
+nix-shell --run "cabal v1-configure --enable-test"  -p '
   haskellPackages.ghcWithPackages (pset: with pset; [
     aeson base-unicode-symbols cmdargs either hspec lens
     tar temporary text th-lift unordered-containers wreq
     yaml zlib
   ])'
-cabal build
+cabal v1-build
 
 # Now include hoogle databases and haddock information.
-nix-shell --run "cabal configure --enable-test"  -p '
+nix-shell --run "cabal v1-configure --enable-test"  -p '
   haskellPackages.ghcWithHoogle (pset: with pset; [
     aeson base-unicode-symbols cmdargs either hspec lens
     tar temporary text th-lift unordered-containers wreq
     yaml zlib
   ])'
-cabal build
+cabal v1-build
 
 # Make this compiler our default GHC.
 mkdir -p ~/.config/nixpkgs

9413a0a776dd6132ac6ee54e609814ff8fce9349
Peter Simons <simons@cryp.to>
2018-11-02 18:04:58 +0100

    02-nix-based-development: configure with --allow-newer to fix the build
    This works around https://github.com/Gabriel439/Haskell-Nix-Derivation-Library/issues/1.

--- 02-nix-based-development/run.sh
+++ 02-nix-based-development/run.sh
@@ -4,18 +4,19 @@ set -eu
 cd "$(dirname "$0")"
 
 # Get the source code of the desired Haskell package.
-rm -rf nix-derivation-1.0.0
-cabal get nix-derivation-1.0.0
-cd nix-derivation-1.0.0
+rm -rf nix-derivation-1.0.1
+cabal get nix-derivation-1.0.1
+cd nix-derivation-1.0.1
 
 # Enter a temporary development environment from Nixpkgs.
-nix-shell "<nixpkgs>" -A haskellPackages.nix-derivation.env --run "cabal configure"
+# The --allow-newer is due to https://github.com/Gabriel439/Haskell-Nix-Derivation-Library/issues/1.
+nix-shell "<nixpkgs>" -A haskellPackages.nix-derivation.env --run "cabal configure --allow-newer"
 cabal build
 
 # Generate a development environment with cabal2nix.
 cabal2nix --shell . >shell.nix
 cabal clean
-nix-shell --run "cabal configure --enable-test"
+nix-shell --run "cabal configure --enable-test --allow-newer"
 cabal test
 
 # Interactive development.

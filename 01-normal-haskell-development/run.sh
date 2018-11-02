189a5b058bd825831a02c22bbc1539040038ee70
Peter Simons <simons@cryp.to>
2018-11-02 17:55:06 +0100

    01-normal-haskell-development: fix stack build
    We are in the middle of a temporal anomaly that needs a hack to work around.

--- 01-normal-haskell-development/run.sh
+++ 01-normal-haskell-development/run.sh
@@ -14,8 +14,13 @@ cabal new-build
 
 # Build it with stack.
 stack config set system-ghc --global true
-stack init
-stack build
+stack config set install-ghc --global false
+# "stack init" currently doesn't work. It detects lts-12.16, but the
+# nixpkgs-unstable channel hasn't updated to ghc-8.4.4 yet. To avoid
+# this issue for the time being, we hard-code an older version.
+echo > stack.yaml "resolver: lts-12.14"
+echo >>stack.yaml "packages:"
+echo >>stack.yaml "  - ."
 
 # Build it with a more recent version of GHC.
 nix-shell -p haskell.compiler.ghc861 --run "cabal new-build"

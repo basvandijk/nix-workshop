8cbcb0046199a32aa670af58c468c4fce193bccd
Peter Simons <simons@cryp.to>
2018-11-02 19:06:40 +0100

    docker-run: pull the latest docker image before starting it

--- docker-run.sh
+++ docker-run.sh
@@ -3,6 +3,9 @@
 # Change into the directory that contains this script.
 cd "$(dirname "$0")" || exit 1
 
+# Make sure we have the latest version of the container.
+docker pull psimons/hex2017
+
 # Enter the virtual environment and map this repository
 # into the container at ~root/workshop.
 exec docker run                 \
